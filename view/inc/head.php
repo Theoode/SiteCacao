208655e8ad76b682e8670bd7f0d59a4781da4604
Theoode <theoquennehenpro@gmail.com>
2024-05-29 14:41:14 +0200

    tailwind

--- view/inc/head.php
+++ view/inc/head.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="src/output.css" rel="stylesheet">
+    <link href="./output.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
